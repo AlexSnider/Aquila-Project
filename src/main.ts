86d71c06a95657044dbc68d3fed9126a04276f22
AlexSnider <alexsnider8@gmail.com>
2024-09-18 11:36:09 -0300

    feat: add database connection and simple server starter

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+import express from "express";
+import ConnectToDatabase from "./database";
+
+const app = express();
+app.use(express.json());
+
+ConnectToDatabase.execute();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
