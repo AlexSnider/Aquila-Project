725e74dee9dee266cbfe0f1dcff444c336a5f5c7
AlexSnider <alexsnider8@gmail.com>
2024-09-18 11:19:00 -0300

    feat: add database connection

---
+++ src/database/index.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+class ConnectToDatabase {
+  async execute() {
+    const mongoUrl = process.env.MONGO_URI as string;
+
+    try {
+      await mongoose.connect(mongoUrl);
+      console.log("MongoDB connected!");
+    } catch (error: any) {
+      console.log(error);
+    }
+  }
+}
+
+export default new ConnectToDatabase();
