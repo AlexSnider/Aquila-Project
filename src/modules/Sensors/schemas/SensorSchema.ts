3f12596ba05849c995a6828b56146ff45ba2bc4d
AlexSnider <alexsnider8@gmail.com>
2024-09-26 11:11:09 -0300

    feat: add timestamps for autoupdate on dates

---
+++ src/modules/Sensors/schemas/SensorSchema.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from "mongoose";
+import { Sensor } from "../entities/Sensor";
+
+const SensorSchema = new Schema<Sensor>(
+  {
+    sensor_name: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: String,
+      required: [true, "user_id is required on sensor creation"],
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<Sensor>("sensors", SensorSchema);
