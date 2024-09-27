da3c031348c72db28d708b98696a898409530a16
AlexSnider <alexsnider8@gmail.com>
2024-09-27 10:30:30 -0300

    feat: add route user-sensors and refacts find to find-sensor

--- src/routes/v1/Sensors/sensorRoute.ts
+++ src/routes/v1/Sensors/sensorRoute.ts
@@ -5,12 +5,14 @@ import findAllController from "../../../modules/v1/Sensors/useCases/findAll/find
 import findByIdController from "../../../modules/v1/Sensors/useCases/findById/findById.controller";
 import updateSensorController from "../../../modules/v1/Sensors/useCases/update/updateSensor.controller";
 import deleteSensorController from "../../../modules/v1/Sensors/useCases/delete/deleteSensor.controller";
+import findByUserIdController from "../../../modules/v1/Sensors/useCases/findByUserId/findByUserId.controller";
 
 const sensorRoutes = Router();
 
 sensorRoutes.post("/", createController.handle);
 sensorRoutes.get("/", paginationMiddleware.execute, findAllController.handle);
-sensorRoutes.get("/find/:id", findByIdController.handle);
+sensorRoutes.get("/find-sensor/:id", findByIdController.handle);
+sensorRoutes.get("/user-sensors/:id", findByUserIdController.handle);
 sensorRoutes.patch("/update/:id", updateSensorController.handle);
 sensorRoutes.delete("/delete/:id", deleteSensorController.handle);
 
