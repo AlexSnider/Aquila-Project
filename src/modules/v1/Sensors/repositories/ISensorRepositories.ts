5e217bc47add49e3c579c8bde21229acc6a01a99
AlexSnider <alexsnider8@gmail.com>
2024-09-27 09:36:05 -0300

    refactor: add return as array

--- src/modules/v1/Sensors/repositories/ISensorRepositories.ts
+++ src/modules/v1/Sensors/repositories/ISensorRepositories.ts
@@ -2,7 +2,7 @@ import { Sensor } from "../entities/Sensor";
 
 export interface ISensorRepositories {
   create(body: Sensor): Promise<void>;
-  findByUserId(user_id: string): Promise<Sensor | null>;
+  findByUserId(user_id: string): Promise<Sensor[] | null>;
   findAll(limit: number, offset: number): Promise<Sensor[]>;
   findById(id: string): Promise<Sensor | null>;
   update(id: string, data: Sensor): Promise<void>;
