e7deb8f22c1b2a889d283228fe09a3754a8273d1
AlexSnider <alexsnider8@gmail.com>
2024-10-01 14:19:25 -0300

    feat: add initial integration tests

---
+++ __tests__/integration/sensors/create.test.ts
@@ -0,0 +1,36 @@
+import supertest from "supertest";
+import app, { initApp, close } from "../../../src/app";
+
+const supertestServer = supertest(app);
+
+beforeAll(async () => {
+  await initApp();
+}, 10000);
+
+afterAll(async () => {
+  await close();
+});
+
+describe("create", () => {
+  it("should create a sensor", async () => {
+    const result = await supertestServer.post("/sensors").send({
+      sensor_name: "swee",
+      user_id: "1",
+      coordinates: [34.65, 456.37],
+    });
+
+    expect(result.statusCode).toBe(201);
+  });
+
+  it("should not create a sensor", async () => {
+    const result = await supertestServer.post("/sensors").send({
+      sensor_name: "swer",
+      user_id: "1",
+      coordinates: [34.65, "ersd"],
+    });
+
+    expect(result.statusCode).toBe(409);
+    expect(result.body).toHaveProperty("errors");
+    console.log(result.body);
+  });
+});
