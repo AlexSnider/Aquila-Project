7cfcb2cf77b7c74520b49b8442e22cfb875a93a1
AlexSnider <alexsnider8@gmail.com>
2024-10-01 14:11:46 -0300

    refactor: removes search for id introducting sensor_name instead

--- src/modules/v1/Sensors/useCases/create/create.service.ts
+++ src/modules/v1/Sensors/useCases/create/create.service.ts
@@ -1,3 +1,4 @@
+import { ConflictError } from "../../../../../helpers/errors/apiErrors";
 import { Sensor } from "../../entities/Sensor";
 import { ISensorRepositories } from "../../repositories/ISensorRepositories";
 import { inject, injectable } from "tsyringe";
@@ -10,12 +11,12 @@ export class CreateService {
   ) {}
 
   async execute(body: Sensor): Promise<void> {
-    const sensorExists = await this.sensorRepository.findById(
-      body._id as unknown as string
+    const sensorExists = await this.sensorRepository.findByName(
+      body.sensor_name as string
     );
 
     if (sensorExists) {
-      throw new Error("Sensor already exists");
+      throw new ConflictError("Sensor already exists");
     }
 
     await this.sensorRepository.create(body);
