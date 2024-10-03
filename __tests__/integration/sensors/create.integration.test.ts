4d5951449fc5062108686f7aa8c982afc168f372
AlexSnider <alexsnider8@gmail.com>
2024-10-03 15:03:38 -0300

    refactor: sensor tests refactored using factories

--- __tests__/integration/sensors/create.integration.test.ts
+++ __tests__/integration/sensors/create.integration.test.ts
@@ -1,120 +1,69 @@
 import supertest from "supertest";
-import app, { initApp, close } from "../../app";
+import app, { initApp, close } from "../../../src/app";
+import {
+  newInvalidSensorBlankField,
+  newInvalidSensorCoordinate,
+  newInvalidSensorExistingName,
+  newInvalidSensorName,
+  newInvalidSensorSchema,
+  newSensor,
+} from "__tests__/factories/sensor.factories";
 
 const supertestServer = supertest(app);
 
 beforeAll(async () => {
   await initApp();
-}, 20000);
+});
 
 afterAll(async () => {
   await close();
 });
 
-describe("Create Sensor", () => {
-  it("Should create a sensor and return status code 201", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test",
-      user_id: "1",
-      coordinates: [6534.65, 2456.37],
-    });
+describe("Sensor Creation", () => {
+  it("should create a sensor and return status 201", async () => {
+    const sensor = newSensor();
+    const result = await supertestServer.post("/sensors").send(sensor);
 
     expect(result.statusCode).toBe(201);
   });
 
-  it("should not create a sensor. Coordinates must be an array of two numbers and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test",
-      user_id: "1",
-      coordinates: [34.65, "test"],
-    });
+  it("should fail when coordinates are not an array of two numbers and return status 409", async () => {
+    const sensor = newInvalidSensorCoordinate();
+    const result = await supertestServer.post("/sensors").send(sensor);
 
     expect(result.statusCode).toBe(409);
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("Should not create a sensor. Missing coordinates and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test",
-      user_id: "1",
-    });
+  it("should fail when a required field is missing and return status 409", async () => {
+    const sensor = newInvalidSensorSchema();
+    const result = await supertestServer.post("/sensors").send(sensor);
 
     expect(result.statusCode).toBe(409);
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("Should not create a sensor. Missing user id and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test",
-      coordinates: [34.65, 34.65],
-    });
+  it("should fail when sensor name is not a string and return status 409", async () => {
+    const sensor = newInvalidSensorName();
+    const result = await supertestServer.post("/sensors").send(sensor);
 
     expect(result.statusCode).toBe(409);
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("Should not create a sensor. Missing sensor name and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      user_id: "1",
-      coordinates: [34.65, 34.65],
-    });
+  it("should fail when sensor has blank fields and return status 409", async () => {
+    const sensor = newInvalidSensorBlankField();
+    const result = await supertestServer.post("/sensors").send(sensor);
 
     expect(result.statusCode).toBe(409);
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("Should not create a sensor. Sensor name already exists and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test",
-      user_id: "1",
-      coordinates: [34.65, 34.65],
-    });
+  it("should fail when sensor name already exists and return status 409", async () => {
+    const sensor = newInvalidSensorExistingName();
+    const result = await supertestServer.post("/sensors").send(sensor);
 
     expect(result.statusCode).toBe(409);
     expect(result.body).toHaveProperty("message");
-  });
-
-  it("Should not create a sensor. Sensor name must be a string and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: 0o12,
-      user_id: "1",
-      coordinates: [34.65, 34.65],
-    });
-
-    expect(result.statusCode).toBe(409);
-    expect(result.body).toHaveProperty("errors");
-  });
-
-  it("Should not create a sensor. User ID must be a string and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test",
-      user_id: 0o12,
-      coordinates: [34.65, 34.65],
-    });
-
-    expect(result.statusCode).toBe(409);
-    expect(result.body).toHaveProperty("errors");
-  });
-
-  it("Should not create a sensor. Coordinates cannot have null values and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test2",
-      user_id: "1",
-      coordinates: [34.65, null],
-    });
-
-    expect(result.statusCode).toBe(409);
-    expect(result.body).toHaveProperty("errors");
-  });
-
-  it("Should not create a sensor. Coordinates cannot have undefined values and return status code 409", async () => {
-    const result = await supertestServer.post("/sensors").send({
-      sensor_name: "test2",
-      user_id: "1",
-      coordinates: [34.65, undefined],
-    });
-
-    expect(result.statusCode).toBe(409);
-    expect(result.body).toHaveProperty("errors");
   });
 });
