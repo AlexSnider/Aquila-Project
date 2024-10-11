474725f6f5eb0d6be98b9636253ba2bc52318bfe
AlexSnider <alexsnider8@gmail.com>
2024-10-11 15:25:35 -0300

    refactor: removes initial configs and refacts descriptions

--- __tests__/integration/sensors/create.integration.test.ts
+++ __tests__/integration/sensors/create.integration.test.ts
@@ -1,5 +1,5 @@
 import supertest from "supertest";
-import app, { initApp, close } from "../../../src/app";
+import app from "../../../src/app";
 import {
   newInvalidSensorBlankField,
   newInvalidSensorCoordinate,
@@ -11,16 +11,8 @@ import {
 
 const supertestServer = supertest(app);
 
-beforeAll(async () => {
-  await initApp();
-}, 15000);
-
-afterAll(async () => {
-  await close();
-});
-
-describe("Sensor Creation", () => {
-  it("should create a sensor and return status 201", async () => {
+describe("POST /sensors - Sensor creation tests", () => {
+  it("should successfully create a sensor and return status 201", async () => {
     const sensor = newSensor();
     const result = await supertestServer.post("/sensors").send(sensor);
 
@@ -32,7 +24,7 @@ describe("Sensor Creation", () => {
     expect(result.body.coordinates.length).toBe(2);
   });
 
-  it("should fail when coordinates are not an array of two numbers and return status 409", async () => {
+  it("should return 409 when coordinates are not an array of two numbers", async () => {
     const sensor = newInvalidSensorCoordinate();
     const result = await supertestServer.post("/sensors").send(sensor);
 
@@ -40,7 +32,7 @@ describe("Sensor Creation", () => {
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("should fail when a required field is missing and return status 409", async () => {
+  it("should return 409 when a required field is missing", async () => {
     const sensor = newInvalidSensorSchema();
     const result = await supertestServer.post("/sensors").send(sensor);
 
@@ -48,7 +40,7 @@ describe("Sensor Creation", () => {
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("should fail when sensor name is not a string and return status 409", async () => {
+  it("should return 409 when sensor name is not a string", async () => {
     const sensor = newInvalidSensorName();
     const result = await supertestServer.post("/sensors").send(sensor);
 
@@ -56,7 +48,7 @@ describe("Sensor Creation", () => {
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("should fail when sensor has blank fields and return status 409", async () => {
+  it("should return 409 when sensor has blank fields", async () => {
     const sensor = newInvalidSensorBlankField();
     const result = await supertestServer.post("/sensors").send(sensor);
 
@@ -64,7 +56,7 @@ describe("Sensor Creation", () => {
     expect(result.body).toHaveProperty("errors");
   });
 
-  it("should fail when sensor name already exists and return status 409", async () => {
+  it("should return 409 when sensor name already exists", async () => {
     const sensor = newInvalidSensorExistingName();
     const result = await supertestServer.post("/sensors").send(sensor);
 
