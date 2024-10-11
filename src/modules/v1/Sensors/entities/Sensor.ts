ba7c66310434f537da3f2ae0f1f2ac95416cadc7
AlexSnider <alexsnider8@gmail.com>
2024-10-11 15:26:50 -0300

    refactor: data types changed and attributes permissions

--- src/modules/v1/Sensors/entities/Sensor.ts
+++ src/modules/v1/Sensors/entities/Sensor.ts
@@ -1,18 +1,18 @@
-import { ObjectId } from "mongoose";
+import { Types } from "mongoose";
 
 export class Sensor {
-  _id?: ObjectId;
-  sensor_name?: string;
-  user_id?: string;
-  coordinates?: [number, number];
-  createdAt?: Date;
-  updatedAt?: Date;
+  _id: Types.ObjectId;
+  sensor_name: string;
+  user_id: string;
+  coordinates: [Number, Number];
+  createdAt: Date;
+  updatedAt: Date;
 
   constructor(
-    sensor_name?: string,
-    user_id?: string,
-    coordinates?: [number, number],
-    _id?: ObjectId
+    sensor_name: string,
+    user_id: string,
+    coordinates: [Number, Number],
+    _id: Types.ObjectId
   ) {
     this._id = _id;
     this.sensor_name = sensor_name;
