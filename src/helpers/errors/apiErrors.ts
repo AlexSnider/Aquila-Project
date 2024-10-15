aa501afc70519582e0fb5bc20e30ccd78039bb56
AlexSnider <alexsnider8@gmail.com>
2024-10-15 12:12:32 -0300

    feat: add new erroe [forbidden]

--- src/helpers/errors/apiErrors.ts
+++ src/helpers/errors/apiErrors.ts
@@ -36,3 +36,9 @@ export class DatabaseError extends ApiError {
     super(message, 409);
   }
 }
+
+export class ForbiddenError extends ApiError {
+  constructor(message: string) {
+    super(message, 403);
+  }
+}
