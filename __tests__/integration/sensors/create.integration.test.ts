f7eaab4a76126f67174625c64bd4cb778bc011ab
AlexSnider <alexsnider8@gmail.com>
2024-10-15 12:07:40 -0300

    style: changes tests order

--- __tests__/integration/sensors/create.integration.test.ts
+++ __tests__/integration/sensors/create.integration.test.ts
@@ -12,18 +12,6 @@ import {
 const supertestServer = supertest(app);
 
 describe("POST /sensors - Sensor creation tests", () => {
-  it("should successfully create a sensor and return status 201", async () => {
-    const sensor = newSensor();
-    const result = await supertestServer.post("/sensors").send(sensor);
-
-    expect(result.statusCode).toBe(201);
-    expect(result.body).toHaveProperty("sensor_name");
-    expect(result.body).toHaveProperty("user_id");
-    expect(result.body).toHaveProperty("coordinates");
-    expect(Array.isArray(result.body.coordinates)).toBe(true);
-    expect(result.body.coordinates.length).toBe(2);
-  });
-
   it("should return 409 when coordinates are not an array of two numbers", async () => {
     const sensor = newInvalidSensorCoordinate();
     const result = await supertestServer.post("/sensors").send(sensor);
@@ -54,6 +42,18 @@ describe("POST /sensors - Sensor creation tests", () => {
 
     expect(result.statusCode).toBe(409);
     expect(result.body).toHaveProperty("errors");
+  });
+
+  it("should successfully create a sensor and return status 201", async () => {
+    const sensor = newSensor();
+    const result = await supertestServer.post("/sensors").send(sensor);
+
+    expect(result.statusCode).toBe(201);
+    expect(result.body).toHaveProperty("sensor_name");
+    expect(result.body).toHaveProperty("user_id");
+    expect(result.body).toHaveProperty("coordinates");
+    expect(Array.isArray(result.body.coordinates)).toBe(true);
+    expect(result.body.coordinates.length).toBe(2);
   });
 
   it("should return 409 when sensor name already exists", async () => {
