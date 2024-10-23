22346c804b048f74c31a95aba55b8f3be15e620d
AlexSnider <alexsnider8@gmail.com>
2024-10-23 14:11:13 -0300

    feat: add health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import sensorRoutes from "./v1/Sensors/sensorRoute";
+import healthCheckRoute from "./health-check/health-check-route/healthCheckRoute";
 
 const router = Router();
 
+router.use("/health-check", healthCheckRoute);
 router.use("/sensors", sensorRoutes);
 
 export default router;
