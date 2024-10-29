4f8456d205e255eea4759f31d4ae85ebaa7e8eb7
AlexSnider <alexsnider8@gmail.com>
2024-10-29 10:14:35 -0300

    feat: add cors middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import sensorRoutes from "./v1/Sensors/sensorRoute";
 import healthCheckRoute from "./health-check/health-check-route/healthCheckRoute";
+import corsMiddleware from "src/middleware/corsMiddleware";
 
 const router = Router();
 
-router.use("/health-check", healthCheckRoute);
-router.use("/sensors", sensorRoutes);
+router.use("/health-check", corsMiddleware.execute, healthCheckRoute);
+router.use("/sensors", corsMiddleware.execute, sensorRoutes);
 
 export default router;
