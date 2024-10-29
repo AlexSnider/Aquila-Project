54ca1aa6b804e42f3ed3a0c45ba10edc11fe6e2d
Alex Oliveira <103783575+AlexSnider@users.noreply.github.com>
2024-10-29 10:23:32 -0300

    Feature/sensors (#26)
    * feat: add health check
    
    * build: add cors
    
    * build: remove auto update for cors depedencie
    
    * feat: add cors
    
    * feat: add cors middleware
    
    * feat: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import "express-async-errors";
 import "reflect-metadata";
 import "../src/helpers/container";
 import express, { json, Express } from "express";
+import router from "../src/routes";
 import ConnectToMongoDatabase from "./database/production";
 import ConnectToTestDatabase from "./database/tests";
-import router from "../src/routes";
 import errorMiddleware from "../src/middleware/errorMiddleware";
+import corsMiddleware from "../src/middleware/corsMiddleware";
 
 const app = express();
 
 app.use(json());
+app.use(corsMiddleware.execute);
 app.use(router);
 app.use(errorMiddleware.execute);
 
