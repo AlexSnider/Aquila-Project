41020bea81aeac5de3ae229e2310167b8979292b
AlexSnider <alexsnider8@gmail.com>
2025-02-04 11:15:49 -0300

    feat: add new endpoints

--- src/routes/v1/Sensors/sensorRoute.ts
+++ src/routes/v1/Sensors/sensorRoute.ts
@@ -1,24 +1,43 @@
 import { Router } from "express";
+import { SensorSchemaJoi } from "../../../modules/v1/Sensors/schemas/joi/SensorSchemajoi";
 import createController from "../../../modules/v1/Sensors/useCases/create/create.controller";
 import paginationMiddleware from "../../../middleware/paginationMiddleware";
-import findAllController from "../../../modules/v1/Sensors/useCases/findAll/findAll.controller";
-import findByIdController from "../../../modules/v1/Sensors/useCases/findById/findById.controller";
+import findAllController from "../../../modules/v1/Sensors/useCases/find/findAll/findAll.controller";
+import findCollectionByUserIdController from "../../../modules/v1/Sensors/useCases/find/findCollectionByUserId/findCollectionByUserId.controller";
+import findGroupsByUserIdAndGroupIdController from "src/modules/v1/Sensors/useCases/find/findGroupsByUserIdAndGroupId/findGroupsByUserIdAndGroupId.controller";
+import findSensorByUserIdAndSensorIdController from "src/modules/v1/Sensors/useCases/find/findSensorByUserIdAndSensorId/findSensorByUserIdAndSensorId.controller";
 import updateSensorController from "../../../modules/v1/Sensors/useCases/update/updateSensor.controller";
 import deleteSensorController from "../../../modules/v1/Sensors/useCases/delete/deleteSensor.controller";
-import findByUserIdController from "../../../modules/v1/Sensors/useCases/findByUserId/findByUserId.controller";
 import validadeSchema from "../../../middleware/schemaValidationMiddleware";
-import { SensorSchemaJoi } from "../../../modules/v1/Sensors/schemas/joi/SensorSchemajoi";
 
 const sensorRoutes = Router();
 
 sensorRoutes.post(
-  "/",
+  "/register",
   validadeSchema.execute(SensorSchemaJoi),
   createController.handle
 );
-sensorRoutes.get("/", paginationMiddleware.execute, findAllController.handle);
-sensorRoutes.get("/:id", findByIdController.handle);
-sensorRoutes.get("/users-sensors/:id", findByUserIdController.handle);
+sensorRoutes.get(
+  "/collections",
+  paginationMiddleware.execute,
+  findAllController.handle
+);
+
+sensorRoutes.get(
+  "/user-collection/:id",
+  findCollectionByUserIdController.handle
+);
+
+sensorRoutes.get(
+  "/:user_id/group-id/:_id",
+  findGroupsByUserIdAndGroupIdController.handle
+);
+
+sensorRoutes.get(
+  "/:user_id/sensor-id/:_id",
+  findSensorByUserIdAndSensorIdController.handle
+);
+
 sensorRoutes.patch("/update/:id", updateSensorController.handle);
 sensorRoutes.delete("/delete/:id", deleteSensorController.handle);
 
