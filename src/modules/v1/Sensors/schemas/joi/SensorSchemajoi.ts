87c168448479f454f28adc54647ef1ac789e65f1
AlexSnider <alexsnider8@gmail.com>
2025-02-04 11:07:27 -0300

    feat: new validations added to joi

--- src/modules/v1/Sensors/schemas/joi/SensorSchemajoi.ts
+++ src/modules/v1/Sensors/schemas/joi/SensorSchemajoi.ts
@@ -1,10 +1,28 @@
 import Joi from "joi";
 
 export const SensorSchemaJoi = Joi.object({
-  sensor_name: Joi.string().min(4).alphanum().required(),
   user_id: Joi.string().uuid().required(),
-  location: Joi.object({
-    type: Joi.string().valid("Point").required(),
-    coordinates: Joi.array().items(Joi.number()).length(2).required(),
-  }).required(),
+  sensor_groups: Joi.array()
+    .items(
+      Joi.object({
+        sensor_group_name: Joi.string().min(4).max(50).strict().required(),
+        sensors: Joi.array()
+          .items(
+            Joi.object({
+              sensor_name: Joi.string().min(4).max(50).strict().required(),
+              location: Joi.object({
+                type: Joi.string().valid("Point").required(),
+                coordinates: Joi.array()
+                  .items(Joi.number().strict().required())
+                  .length(2)
+                  .required(),
+              }).required(),
+            })
+          )
+          .max(25)
+          .required(),
+      })
+    )
+    .max(10)
+    .required(),
 });
