b1a1784c16ec4cfc9dc2baced7c3d919ddb93f0e
AlexSnider <alexsnider8@gmail.com>
2025-02-04 11:12:10 -0300

    feat: add findAll controller and service refactored

---
+++ src/modules/v1/Sensors/useCases/find/findAll/findAll.service.ts
@@ -0,0 +1,20 @@
+import { injectable, inject } from "tsyringe";
+import { ISensorRepositories } from "../../../repositories/ISensorRepositories";
+import { Sensor } from "../../../entities/Sensor";
+import { NotFoundError } from "src/helpers/errors/apiErrors";
+
+@injectable()
+export class FindAllService {
+  constructor(
+    @inject("SensorRepositories")
+    private sensorRepository: ISensorRepositories
+  ) {}
+
+  async execute(limit: number, offset: number): Promise<Sensor[]> {
+    const sensors = await this.sensorRepository.findAll(limit, offset);
+    if (sensors.length === 0) {
+      throw new NotFoundError("No collections found");
+    }
+    return sensors;
+  }
+}
