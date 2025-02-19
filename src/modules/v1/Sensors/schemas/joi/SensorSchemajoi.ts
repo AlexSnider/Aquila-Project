73afbeffa4224b07ba4e58b5203c0cc7d65d8162
AlexSnider <alexsnider8@gmail.com>
2025-02-19 08:30:23 -0300

    feat: add new schema validations

--- src/modules/v1/Sensors/schemas/joi/SensorSchemajoi.ts
+++ src/modules/v1/Sensors/schemas/joi/SensorSchemajoi.ts
@@ -26,3 +26,29 @@ export const SensorSchemaJoi = Joi.object({
     .max(10)
     .required(),
 });
+
+export const SensorGroupSchemaJoi = Joi.object({
+  sensor_group_name: Joi.string().min(4).max(50).strict().required(),
+});
+
+export const SensorDataSchemaJoi = Joi.object({
+  sensor_name: Joi.string().min(4).max(50).strict().required(),
+  location: Joi.object({
+    type: Joi.string().valid("Point").required(),
+    coordinates: Joi.array()
+      .items(Joi.number().strict().required())
+      .length(2)
+      .required(),
+  }).required(),
+});
+
+export const SensorInsertDataSchemaJoi = Joi.object({
+  sensor_name: Joi.string().min(4).max(50).strict().required(),
+  coordinates: Joi.object({
+    type: Joi.string().valid("Point").strict().required(),
+    coordinates: Joi.array()
+      .items(Joi.number().strict().required())
+      .length(2)
+      .required(),
+  }),
+});
