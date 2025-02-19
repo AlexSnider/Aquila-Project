46431c2df7e5fc75517abee214b48c2f76733955
AlexSnider <alexsnider8@gmail.com>
2025-02-19 08:35:25 -0300

    feat: add new routes and refacts original routes

--- src/routes/v1/Sensors/sensorRoute.ts
+++ src/routes/v1/Sensors/sensorRoute.ts
@@ -1,57 +1,87 @@
 import { Router } from "express";
-import { SensorSchemaJoi } from "../../../modules/v1/Sensors/schemas/joi/SensorSchemajoi";
-import createController from "../../../modules/v1/Sensors/useCases/create/create.controller";
+import {
+  SensorDataSchemaJoi,
+  SensorGroupSchemaJoi,
+  SensorInsertDataSchemaJoi,
+  SensorSchemaJoi,
+} from "../../../modules/v1/Sensors/schemas/joi/SensorSchemajoi";
+import createController from "../../../modules/v1/Sensors/useCases/create/createNewSensor.controller";
 import paginationMiddleware from "../../../middleware/paginationMiddleware";
 import findAllController from "../../../modules/v1/Sensors/useCases/find/findAll/findAll.controller";
 import findCollectionByUserIdController from "../../../modules/v1/Sensors/useCases/find/findCollectionByUserId/findCollectionByUserId.controller";
 import findGroupsByUserIdAndGroupIdController from "src/modules/v1/Sensors/useCases/find/findGroupsByUserIdAndGroupId/findGroupsByUserIdAndGroupId.controller";
 import findSensorByUserIdAndSensorIdController from "src/modules/v1/Sensors/useCases/find/findSensorByUserIdAndSensorId/findSensorByUserIdAndSensorId.controller";
-import updateSensorController from "../../../modules/v1/Sensors/useCases/update/updateSensor.controller";
 import validadeSchema from "../../../middleware/schemaValidationMiddleware";
 import deleteByUserIdController from "src/modules/v1/Sensors/useCases/delete/deleteByUserId/deleteByUserId.controller";
 import deleteByUserIdAndGroupIdController from "src/modules/v1/Sensors/useCases/delete/deleteByUserIdAndGroupId/deleteByUserIdAndGroupId.controller";
 import deleteByUserIdAndSensorIdController from "src/modules/v1/Sensors/useCases/delete/deleteByUserIdAndSensorId/deleteByUserIdAndSensorId.controller";
+import updateGroupNameController from "src/modules/v1/Sensors/useCases/update/updateGroupNameByUserId/updateGroupNameByUserId.controller";
+import updateSensorDataController from "src/modules/v1/Sensors/useCases/update/updateSensorDataByUserId/updateSensorDataByUserId.controller";
+import insertGroupByUserIdController from "src/modules/v1/Sensors/useCases/update/insertGroupByUserId/insertGroupByUserId.controller";
+import insertSensorByUserIdController from "src/modules/v1/Sensors/useCases/update/insertSensorByUserId/insertSensorByUserId.controller";
 
 const sensorRoutes = Router();
 
 sensorRoutes.post(
-  "/register",
+  "/new-sensor",
   validadeSchema.execute(SensorSchemaJoi),
   createController.handle
 );
 sensorRoutes.get(
-  "/collections",
+  "/all",
   paginationMiddleware.execute,
   findAllController.handle
 );
 
-sensorRoutes.get(
-  "/user-collection/:id",
-  findCollectionByUserIdController.handle
-);
+sensorRoutes.get("/user-id/:id", findCollectionByUserIdController.handle);
 
 sensorRoutes.get(
-  "/:user_id/group-id/:_id",
+  "/user-id/:user_id/group-id/:_id",
   findGroupsByUserIdAndGroupIdController.handle
 );
 
 sensorRoutes.get(
-  "/:user_id/sensor-id/:_id",
+  "/user-id/:user_id/sensor-id/:_id",
   findSensorByUserIdAndSensorIdController.handle
 );
 
-sensorRoutes.delete("/:user_id", deleteByUserIdController.handle);
+sensorRoutes.delete(
+  "/delete/user-id/:user_id",
+  deleteByUserIdController.handle
+);
 
 sensorRoutes.delete(
-  "/user_id/:user_id/group-id/:_id",
+  "/delete/user-id/:user_id/group-id/:_id",
   deleteByUserIdAndGroupIdController.handle
 );
 
 sensorRoutes.delete(
-  "/user_id/:user_id/sensor-id/:_id",
+  "/delete/user-id/:user_id/sensor-id/:_id",
   deleteByUserIdAndSensorIdController.handle
 );
 
-sensorRoutes.patch("/update/:id", updateSensorController.handle);
+sensorRoutes.patch(
+  "/update/user-id/:user_id/group-id/:_id",
+  validadeSchema.execute(SensorGroupSchemaJoi),
+  updateGroupNameController.handle
+);
+
+sensorRoutes.patch(
+  "/update/user-id/:user_id/sensor-id/:_id",
+  validadeSchema.execute(SensorDataSchemaJoi),
+  updateSensorDataController.handle
+);
+
+sensorRoutes.put(
+  "/insert-group/user-id/:user_id",
+  validadeSchema.execute(SensorGroupSchemaJoi),
+  insertGroupByUserIdController.handle
+);
+
+sensorRoutes.put(
+  "/insert-sensor/user-id/:user_id/group-id/:_id",
+  validadeSchema.execute(SensorInsertDataSchemaJoi),
+  insertSensorByUserIdController.handle
+);
 
 export default sensorRoutes;
