26e89907a4cf567fdee30312f8f89332f25c02ce
AlexSnider <alexsnider8@gmail.com>
2025-02-19 08:33:54 -0300

    feat: add new update and insert methods and files

---
+++ src/modules/v1/Sensors/useCases/update/updateGroupNameByUserId/updateGroupNameByUserId.service.ts
@@ -0,0 +1,35 @@
+import { NotFoundError } from "src/helpers/errors/apiErrors";
+import { ISensorRepositories } from "../../../repositories/ISensorRepositories";
+import { inject, injectable } from "tsyringe";
+import { Types } from "mongoose";
+
+interface IUpdateSensorRequest {
+  user_id: string;
+  sensor_groups: Array<{ _id: Types.ObjectId; sensor_group_name: string }>;
+}
+
+@injectable()
+export class UpdateGroupNameService {
+  constructor(
+    @inject("SensorRepositories")
+    private sensorRepository: ISensorRepositories
+  ) {}
+
+  async execute(body: IUpdateSensorRequest): Promise<void> {
+    const groupExists =
+      await this.sensorRepository.findGroupsByUserIdAndGroupId(
+        body.user_id,
+        body.sensor_groups[0]._id
+      );
+
+    if (!groupExists || groupExists.length === 0) {
+      throw new NotFoundError("Group not found");
+    }
+
+    await this.sensorRepository.updateGroupName(
+      body.user_id,
+      body.sensor_groups[0]._id,
+      body.sensor_groups[0].sensor_group_name
+    );
+  }
+}
