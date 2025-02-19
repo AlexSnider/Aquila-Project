725d15b887a5557e8b754797507d491368b2069e
AlexSnider <alexsnider8@gmail.com>
2025-02-19 08:31:52 -0300

    feat: add new create sensor files and methods

---
+++ src/modules/v1/Sensors/useCases/create/createNewSensor.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateNewSensorService } from "./createNewSensor.service";
+
+class CreateNewSensorController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const body = request.body;
+
+    const createService = container.resolve(CreateNewSensorService);
+
+    const sensor = await createService.execute(body);
+
+    return response.status(201).send(sensor);
+  }
+}
+
+export default new CreateNewSensorController();
--- src/modules/v1/Sensors/useCases/create/createNewSensor.service.ts
+++ src/modules/v1/Sensors/useCases/create/createNewSensor.service.ts
@@ -1,3 +1,4 @@
+import { ConflictError } from "src/helpers/errors/apiErrors";
 import { Sensor } from "../../entities/Sensor";
 import { ISensorRepositories } from "../../repositories/ISensorRepositories";
 import { inject, injectable } from "tsyringe";
@@ -14,24 +15,41 @@ interface ICreateSensorRequest {
 }
 
 @injectable()
-export class CreateService {
+export class CreateNewSensorService {
   constructor(
     @inject("SensorRepositories")
     private sensorRepository: ISensorRepositories
   ) {}
 
   async execute(body: ICreateSensorRequest): Promise<Sensor> {
-    const sensorDocument = new Sensor({
+    const userExists = await this.sensorRepository.findCollectionByUserId(
+      body.user_id
+    );
+
+    if (userExists && userExists.length > 0) {
+      throw new ConflictError("User already has a sensor collection");
+    }
+
+    if (
+      body.sensor_groups.length > 1 ||
+      body.sensor_groups[0].sensors.length > 1
+    ) {
+      throw new ConflictError(
+        "Only one group and sensor is allowed when creating a new sensor collection"
+      );
+    }
+
+    const sensorData = new Sensor({
       user_id: body.user_id,
       sensor_groups: body.sensor_groups,
     });
 
-    const createSensor = await this.sensorRepository.create(sensorDocument);
+    const createSensor = await this.sensorRepository.create(sensorData);
 
     if (!createSensor) {
       throw new Error("Error creating sensor");
     }
 
-    return sensorDocument;
+    return sensorData;
   }
 }
