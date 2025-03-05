246c59fd79a17e2de1b8486ce6d63932315ac624
AlexSnider <alexsnider8@gmail.com>
2025-03-05 10:41:27 -0300

    feat: add error middleware to router file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import corsMiddleware from "../middleware/corsMiddleware";
+import errorMiddleware from "../middleware/errorMiddleware";
 import sensorRoutes from "./v1/Sensors/sensorRoute";
 import healthCheckRoute from "./health-check/healthCheckRoute";
 import DocsRoute from "./docs/docsRoute";
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/api-docs", corsMiddleware.execute, DocsRoute);
 router.use("/health-check", corsMiddleware.execute, healthCheckRoute);
 router.use("/api/v1/sensors", corsMiddleware.execute, sensorRoutes);
+router.use(errorMiddleware.execute);
 
 export default router;
