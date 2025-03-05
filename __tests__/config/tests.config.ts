44931a095fd9c6407849b6eeaef8eb9c051b3e27
AlexSnider <alexsnider8@gmail.com>
2025-03-05 10:25:55 -0300

    feat: add test container logic

--- __tests__/config/tests.config.ts
+++ __tests__/config/tests.config.ts
@@ -1,9 +1,32 @@
-import { initApp, close } from "../../src/app";
+import { GenericContainer, StartedTestContainer } from "testcontainers";
+import mongoose from "mongoose";
+
+let mongoContainer: StartedTestContainer;
 
 beforeAll(async () => {
-  await initApp();
-}, 100000);
+  mongoContainer = await new GenericContainer("mongo:latest")
+    .withExposedPorts(27017)
+    .start();
+
+  const host = mongoContainer.getHost();
+  const port = mongoContainer.getMappedPort(27017);
+  const mongoUrl = `mongodb://${host}:${port}/testdb`;
+
+  process.env.DB_URL = mongoUrl;
+
+  await mongoose.connect(mongoUrl);
+  console.log("Test Container Up.");
+}, 60000);
 
 afterAll(async () => {
-  await close();
-}, 100000);
+  if (mongoose.connection.readyState === 1) {
+    await mongoose.connection.dropDatabase();
+  }
+
+  await mongoose.disconnect();
+
+  if (mongoContainer) {
+    await mongoContainer.stop();
+    console.log("Test Container Down.");
+  }
+}, 30000);
