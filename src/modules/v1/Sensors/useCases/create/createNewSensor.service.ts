c224729cf4a82953a97c4f1bb82252c048d49534
AlexSnider <alexsnider8@gmail.com>
2025-03-05 10:39:24 -0300

    refactor: move logic [find user] to repository

--- src/modules/v1/Sensors/useCases/create/createNewSensor.service.ts
+++ src/modules/v1/Sensors/useCases/create/createNewSensor.service.ts
@@ -1,10 +1,14 @@
-import { ConflictError } from "src/helpers/errors/apiErrors";
+import {
+  ConflictError,
+  SensorCreationError,
+} from "../../../../../helpers/errors/apiErrors";
 import { Sensor } from "../../entities/Sensor";
 import { ISensorRepositories } from "../../repositories/ISensorRepositories";
 import { inject, injectable } from "tsyringe";
+import { UUIDTypes } from "node_modules/uuid/dist/cjs";
 
 interface ICreateSensorRequest {
-  user_id: string;
+  user_id: UUIDTypes;
   sensor_groups: {
     sensor_group_name: string;
     sensors: {
@@ -22,21 +26,11 @@ export class CreateNewSensorService {
   ) {}
 
   async execute(body: ICreateSensorRequest): Promise<Sensor> {
-    const userExists = await this.sensorRepository.findCollectionByUserId(
-      body.user_id
-    );
+    const sensorCollectionExists =
+      await this.sensorRepository.findCollectionByUserId(body.user_id);
 
-    if (userExists && userExists.length > 0) {
-      throw new ConflictError("User already has a sensor collection");
-    }
-
-    if (
-      body.sensor_groups.length > 1 ||
-      body.sensor_groups[0].sensors.length > 1
-    ) {
-      throw new ConflictError(
-        "Only one group and sensor is allowed when creating a new sensor collection"
-      );
+    if (sensorCollectionExists && sensorCollectionExists.length > 0) {
+      throw new ConflictError("Sensor collection already exists");
     }
 
     const sensorData = new Sensor({
@@ -44,12 +38,12 @@ export class CreateNewSensorService {
       sensor_groups: body.sensor_groups,
     });
 
-    const createSensor = await this.sensorRepository.create(sensorData);
+    const createdSensor = await this.sensorRepository.create(sensorData);
 
-    if (!createSensor) {
-      throw new Error("Error creating sensor");
+    if (!createdSensor) {
+      throw new SensorCreationError("Unexpected error creating sensor");
     }
 
-    return sensorData;
+    return createdSensor;
   }
 }
