7e2c54396086fa17ca8dc4eefa8c54bea4440d9a
AlexSnider <alexsnider8@gmail.com>
2025-03-05 10:31:52 -0300

    refactor: alters logic when testing or starting the application [DB changes]

--- src/app.ts
+++ src/app.ts
@@ -3,33 +3,35 @@ import "reflect-metadata";
 import "../src/helpers/container";
 import express, { json, Express } from "express";
 import router from "./routes";
+import mongoose from "mongoose";
 import ConnectToMongoDatabase from "./database/production";
-import ConnectToTestDatabase from "./database/tests";
-import errorMiddleware from "./middleware/errorMiddleware";
+import errorMiddleware  from "./middleware/errorMiddleware";
 import corsMiddleware from "./middleware/corsMiddleware";
 
 const app = express();
 
 app.use(json());
+app.use(errorMiddleware.execute);
 app.use(corsMiddleware.execute);
 app.use(router);
-app.use(errorMiddleware.execute);
 
 export async function initApp(): Promise<Express> {
-  if (process.env.NODE_ENV === "test") {
-    await ConnectToTestDatabase.execute();
-  } else {
+  const mongoUrl: string | undefined = process.env.DB_URL;
+
+  if (!mongoUrl) {
     await ConnectToMongoDatabase.execute();
+    console.log("Using Production Database...");
   }
-  
-  return Promise.resolve(app);
+
+  if (mongoUrl) {
+    console.log("Using Test Database...");
+    await mongoose.connect(mongoUrl as string);
+  }
+
+  return app;
 }
 
 export async function close(): Promise<void> {
-  if (process.env.NODE_ENV === "test") {
-    await ConnectToTestDatabase.disconnect();
-  }
-
   return await ConnectToMongoDatabase.disconnect();
 }
 
