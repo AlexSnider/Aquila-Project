e5877b5ee241bc83bf4da362616ae4e394dbcaef
AlexSnider <alexsnider8@gmail.com>
2025-03-10 10:05:00 -0300

    refactor: desables eslint on limit and offset clauses

--- src/middleware/paginationMiddleware.ts
+++ src/middleware/paginationMiddleware.ts
@@ -4,7 +4,9 @@ class PaginationMiddleware {
   execute(request: Request, response: Response, next: NextFunction): void {
     const { limit, offset } = request.query;
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     limit ? Number(limit) : 10;
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     offset ? Number(offset) : 0;
 
     response.locals.pagination = { limit, offset };
