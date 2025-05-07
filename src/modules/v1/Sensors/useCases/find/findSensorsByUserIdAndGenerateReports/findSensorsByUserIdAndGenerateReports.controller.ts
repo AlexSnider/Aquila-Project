f0b7f9b48728c25f85dd1aa39bbf781c42450b8b
AlexSnider <alexsnider8@gmail.com>
2025-05-07 10:55:59 -0300

    feat: add new verifications to report controller

--- src/modules/v1/Sensors/useCases/find/findSensorsByUserIdAndGenerateReports/findSensorsByUserIdAndGenerateReports.controller.ts
+++ src/modules/v1/Sensors/useCases/find/findSensorsByUserIdAndGenerateReports/findSensorsByUserIdAndGenerateReports.controller.ts
@@ -2,23 +2,33 @@ import { container } from "tsyringe";
 import { Request, Response } from "express";
 import { FindCollectionByUserIdService } from "../findCollectionByUserId/findCollectionByUserId.service";
 import { generateSensorsReport } from "src/utils/generateSensorsReport";
+import { NotFoundError } from "src/helpers/errors/apiErrors";
 
 class FindSensorsByUserIdAndGenerateReportsController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const user_id = request.params.user_id;
+    try {
+      const user_id = request.params.user_id;
 
-    const findCollectionByUserId = container.resolve(
-      FindCollectionByUserIdService
-    );
+      const findCollectionByUserId = container.resolve(
+        FindCollectionByUserIdService
+      );
 
-    const sensor = await findCollectionByUserId.execute(user_id);
+      const sensor = await findCollectionByUserId.execute(user_id);
 
-    const pdfBuffer = await generateSensorsReport(sensor);
+      const pdfBuffer = await generateSensorsReport(sensor);
 
-    response.setHeader("Content-Type", "application/pdf");
-    response.setHeader("Content-Disposition", "inline; filename=relatorio.pdf");
+      response.setHeader("Content-Type", "application/pdf");
+      response.setHeader("Content-Disposition", "inline; filename=report.pdf");
 
-    return response.status(200).send(pdfBuffer);
+      return response.status(200).send(pdfBuffer);
+    } catch (error: unknown) {
+      if (error instanceof NotFoundError) {
+        return response
+          .status(error.statusCode)
+          .json({ message: error.message });
+      }
+      return response.status(500).json({ message: "Unexpected error" });
+    }
   }
 }
 
