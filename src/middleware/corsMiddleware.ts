33ab7aa5dfd626a3f2567f125a4a4f20bd5e1a54
AlexSnider <alexsnider8@gmail.com>
2025-05-19 11:14:50 -0300

    feat: add cors new specified origins

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -1,16 +1,30 @@
 import { Request, Response, NextFunction } from "express";
 
+const allowedOrigins = [
+  "https://aquila-project.onrender.com",
+  "http://localhost:3000",
+];
+
 class CorsMiddleware {
   execute(request: Request, response: Response, next: NextFunction): void {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE, PUT");
+    const origin = request.headers.origin;
+
+    if (origin && allowedOrigins.includes(origin)) {
+      response.header("Access-Control-Allow-Origin", origin);
+    }
+
+    response.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, POST, DELETE, PUT"
+    );
     response.header(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization, Content-Length, X-Requested-With"
     );
 
     if (request.method === "OPTIONS") {
-      response.status(200).send();
+      response.sendStatus(200);
+      return;
     }
 
     return next();
